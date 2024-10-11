cb480130f60b41bb55256635a49c55b935256614
Евгений <evgeniibelov1401@gmail.com>
2024-10-11 19:34:32 +0300

    нужно что то сделать app.UseMiddleware<LoggingMiddleware>();

--- Startup.cs
+++ Startup.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
@@ -13,6 +14,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
 
 
 namespace MvcStartApp
@@ -51,7 +53,6 @@ namespace MvcStartApp
             }
             app.UseHttpsRedirection();
             app.UseStaticFiles();
-
             app.UseRouting();
 
             app.UseAuthorization();
