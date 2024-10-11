dfab1e10338a4f7d64cb3fb07a14a595ac9bbf3d
Евгений <evgeniibelov1401@gmail.com>
2024-10-11 14:14:21 +0300

    Создал таблицу в БД Requests

--- Models/BlogContext.cs
+++ Models/BlogContext.cs
@@ -11,6 +11,8 @@ namespace MvcStartApp.Models
         /// Ссылка на таблицу UserPosts
         public DbSet<UserPost> UserPosts { get; set; }
 
+        public DbSet<Request> Requests { get; set; }
+
         // Логика взаимодействия с таблицами в БД
         public BlogContext(DbContextOptions<BlogContext> options) : base(options)
         {
